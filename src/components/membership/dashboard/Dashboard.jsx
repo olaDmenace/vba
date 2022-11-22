5d39b305131e5d2c54b9898bf78792bace245308
olaDmenace <olayinkafagbenro@gmail.com>
2022-11-22 18:03:51 +0100

    Dashboard done!

--- src/components/membership/dashboard/Dashboard.jsx
+++ src/components/membership/dashboard/Dashboard.jsx
@@ -41,6 +41,27 @@ const Dashboard = () => {
                     </svg>
                 </div>
             </div>
+            <div className='text-white/70 space-y-6'>
+                <h3>Trade Managers</h3>
+                <div className='md:flex'>
+                    <div className='border rounded divide-y lg:basis-1/2'>
+                        <div className='p-5 space-y-2'>
+                            <h3>Trades</h3>
+                            <p>Trade Managr Signal Groups You Manage</p>
+                        </div>
+                        <div className='p-5 space-y-10'>
+                            <h5>Welcome Johnson</h5>
+                            <p>You do not manage any group yet. You could become a trade manager by creating signal groups</p>
+                            <button className='border h-12 px-4 rounded border-primary hover:bg-primary active:bg-primary-dark ease-in-out transition-colors duration-500'>Become a Trade Manager</button>
+                        </div>
+                    </div>
+                    <div className='basis-1/2 grid items-center justify-items-center h-60 md:h-auto'>
+                        <p className='text-primary hover:text-primary-light active:text-primary-dark'>Become a Trade Manager</p>
+                        <p>Or</p>
+                        <p className='text-primary hover:text-primary-light active:text-primary-dark'>Connect With Other Trade Managers</p>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
